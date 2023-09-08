6b919c591215e7319a828bd5a496d932ccc09ee0
MdSharib <mdsharib15041998@gmail.com>
2023-09-08 23:23:02 +0530

    react: initialized app; header: navbar mui

---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./styles.css"
+import TemporaryDrawer from './drawer';
+
+const Header = () => {
+  return (
+    <div className='navbar'>
+      <h1 className='logo'>CryptoTracker<span style={{color: "var(--blue)"}}>.</span></h1>
+      <div className='links'>
+        <a href='/'><p className='link'>Home</p></a>
+        <a href='/'><p className='link'>Compare</p></a>
+        <a href='/'><p className='link'>Watchlist</p></a>
+        <a href='/'><p className='link'>Dashboard</p></a>
+      </div>
+      <div className='mobile-drawer'>
+        <TemporaryDrawer />
+      </div>
+    </div>
+  )
+}
+
+
+export default Header;
